adfc209e8fe1e1f6c113735438d71432c2e4c023
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-06-21 00:08:00 +0530

    initiated uni of melbourne

--- redo/exp.py
+++ redo/exp.py
@@ -1,13 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import json
 
-url = "https://www.kcl.ac.uk/people/alfie-abdul-rahman"
+def fetch_and_parse(url):
+    # Fetch the HTML page
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-r = requests.get(url)
-soup = BeautifulSoup(r.text, "html.parser")
+    # Extract meta data
+    meta_data = {meta['name']: meta['content'] for meta in soup.find_all("meta") if 'name' in meta.attrs and 'content' in meta.attrs}
 
-websites = soup.find('a', string=lambda text: text and ('Personal website' in text or 'Research Profile' in text))
+    # Fetch JavaScript file
+    js_url = "https://www.sydney.edu.au/engineering/about/our-people/academic-staff/xiu-wang.html//etc.clientlibs/corporate-commons/clientlibs/translation.15b6fc27cccbddb5fb7b88a7c6a2c7e4.js"
+    js_response = requests.get(js_url)
+    js_content = js_response.text
 
-if websites:
-    print(websites['href'])
+    # Extract JSON data from JavaScript content
+    start_index = js_content.find('({')
+    end_index = js_content.rfind('});')
+
+    if start_index != -1 and end_index != -1:
+        data_str = js_content[start_index:end_index + 2]
+        try:
+            js_data = json.loads(data_str)
+        except json.JSONDecodeError:
+            js_data = None
+    else:
+        js_data = None
+
+    return meta_data, js_data
+
+# Example usage
+url = "https://www.sydney.edu.au/engineering/about/our-people/academic-staff/xiu-wang.html"
+meta_data, js_data = fetch_and_parse(url)
+print("Meta Data:", meta_data)
+print("JavaScript Data:", js_data)
