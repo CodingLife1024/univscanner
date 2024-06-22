ad1a3b4f02921e7d5e55c32ff3c8c808d19ef3d1
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-06-23 01:07:59 +0530

    selenium

--- redo/exp.py
+++ redo/exp.py
@@ -1,12 +1,37 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
 
-def exp():
-    url = "https://prism-pages.vercel.app/"
+# Path to the ChromeDriver executable
+chrome_driver_path = "univscanner/selenium_chromedriver/chromedriver.exe"
 
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
+# Create a Service object
+service = Service(executable_path=chrome_driver_path)
 
-    print(soup.prettify())
+# Create an Options object
+options = Options()
 
-exp()
+# Set the User-Agent string
+user_agent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36"
+options.add_argument(f"user-agent={user_agent}")
+
+# Disable WebDriver flags
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--disable-blink-features=AutomationControlled')
+
+# Initialize the Chrome WebDriver
+driver = webdriver.Chrome(service=service, options=options)
+
+# Add a delay before navigating to the URL
+time.sleep(5)
+
+# Navigate to the specified URL
+driver.get("https://findanexpert.unimelb.edu.au/profile/1601-atif-ahmad")
+
+# Print the page source
+print(driver.page_source)
+
+# Quit the WebDriver
+driver.quit()
