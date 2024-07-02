1bbfa1c03ffc3fea2ba4241a4cd9a074d7fe8c32
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-07-03 00:34:11 +0530

    jjj

--- redo/delft_uni_tech.py
+++ redo/delft_uni_tech.py
@@ -3,6 +3,14 @@ import re
 from bs4 import BeautifulSoup
 import google_scholar
 
+u_name = "Delft University of Technology"
+country = "Netherlands"
+
+def decode_email(encoded_email):
+    """Decode the email address from the HTML character codes"""
+    decoded_email = ''.join([chr(int(code)) for code in re.findall(r'&#(\d+);', encoded_email)])
+    return decoded_email
+
 def delft_uni_tech():
     url = "https://www.tudelft.nl/en/eemcs/the-faculty/professors"   # homepage url
     r = requests.get(url)
@@ -13,6 +21,32 @@ def delft_uni_tech():
 
     faculty_data = []
 
-    dd = soup.find('article', {'class':'md-9'})
+    professor_blocks = soup.find_all("div", class_="profile theme-blue hoverableBlock hoverableBlock--green hoverableBlock--hasLink")
 
-    
+    for block in professor_blocks:
+        name_element = block.find("h3")
+        if name_element:
+            name = re.sub(r'Prof\.dr\.ir\.|Prof\.dr\.', '', name_element.text).strip()
+            profile_link = block.find("a", href=True)["href"] if block.find("a", href=True) else None
+
+            # print(name, profile_link)
+
+            if profile_link:
+                new_r = requests.get(profile_link)
+                new_soup = BeautifulSoup(new_r.text, "html.parser")
+
+                email = new_soup.find('a', class_='i-mail')['href'][7:] if new_soup.find('a', class_='i-mail') else None
+
+                expertise = ""
+
+                expertise_element = new_soup.find_all("a", class_="btn btn--orange btn--borderBox color-white btn--single")
+                for element in expertise_element:
+                    expertise += element.text.strip().lower()
+
+
+                print(name, profile_link, email, '\n\n', expertise, '\n\n')
+
+
+
+delft_uni_tech()
+
