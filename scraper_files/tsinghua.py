07fef3a21774cc686de19a56fd2fa8bef5909bfd
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-08-03 05:14:49 +0530

    added concurrency to tsingua

--- scraper_files/tsinghua.py
+++ scraper_files/tsinghua.py
@@ -1,9 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import concurrent.futures
 
 university = "Tsinghua University"
 country = "China"
+
+def fetch_faculty_data(faculty):
+    keyword_list = ["operating system", "robotics", "kernel", "embedded system", "hardware", "computer architecture",
+                    "distributed system", "computer organization", "vlsi", "computer and system",
+                    "human-computer interaction", "human computer"]
+
+    name = faculty.find('a').text
+    site = "https://www.cs.tsinghua.edu.cn/csen" + faculty.find('a')['href'][2:]
+    email = faculty.find_all('p')[2].text
+
+    new_r = requests.get(site)
+    found_keyword = any(re.search(re.escape(keyword), new_r.text) for keyword in keyword_list)
+
+    if found_keyword:
+        return [university, country, name, email, site]
+    return None
 
 def tsinghua():
     url = "https://www.cs.tsinghua.edu.cn/csen/Faculty/Full_time_Faculty.htm"
@@ -13,36 +30,19 @@ def tsinghua():
 
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    faculty_data = []
-
-    # Find all divs with class 'card card--person'
     faculty_cards = soup.find_all('div', class_='text')
 
-    keyword_list = ["operating system", "robotics", "kerrnel", "embedded system", "hardware", "computer architecture", "distributed system", "computer organization", "vlsi", "computer and system", "human-computer interaction", "human computer"]
+    faculty_data = []
 
-    for faculty in faculty_cards:
-        name = faculty.find('a').text
-        site = "https://www.cs.tsinghua.edu.cn/csen" + faculty.find('a')['href'][2:]
-        email = faculty.find_all('p')[2].text
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(fetch_faculty_data, faculty_cards))
 
-        # print(name)
-        # print(site)
-        # print(email)
+    faculty_data = [result for result in results if result]
 
-        new_r = requests.get(site)
-        new_soup = BeautifulSoup(new_r.text, "html.parser")
-
-        found_keyword = any(re.search(re.escape(keyword), new_r.text) for keyword in keyword_list)
-
-        if found_keyword :
-
-            faculty_data.append([university, country, name, email, site])
-            print([university, country, name, email, site])
+    for data in faculty_data:
+        print(data)
 
     print()
     print("Tsinghua University done...")
     print()
     return faculty_data
-
-
-# tsinghua()
