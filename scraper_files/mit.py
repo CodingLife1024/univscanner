49b5dea95c27a24ab4396a973d480887b816047a
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-08-04 06:58:02 +0530

    added personal website and parallelism in mit file

--- scraper_files/mit.py
+++ scraper_files/mit.py
@@ -1,56 +1,68 @@
 from bs4 import BeautifulSoup
 import requests
-import re
+import concurrent.futures
+from components.google_scholar import get_scholar_profile
 
 university = "MIT"
 country = "USA"
 
-def mit():
-    url = "https://www.eecs.mit.edu/role/faculty/"
+def get_faculty_data(name, base_url, headers, faculty_data):
+    response = requests.get(base_url, headers=headers)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'html.parser')
 
+    element = soup.find('a', class_="people-index-image", title=name)
+    if not element:
+        return
+
+    text_content = element.get('title')
+    href = element.get('href')
+    indiv_url = href if href.startswith('http') else base_url + href
+
+    new_response = requests.get(indiv_url, headers=headers)
+    new_html_content = new_response.text
+    new_soup = BeautifulSoup(new_html_content, 'html.parser')
+
+    extract_email = new_soup.find_all('a', href=lambda href: href and href.startswith('mailto:'))
+    email = extract_email[0].get_text() if extract_email else 'No Email Found'
+
+    extract_research = new_soup.find_all('a', href=lambda href: href and href.startswith('/people/?fwp_research'))
+    research_list = [link.get_text() for link in extract_research]
+
+    pers_url = new_soup.find('a', href=lambda href: href and href.startswith('http'), string='Website')
+    if pers_url:
+        pers_url = pers_url.get('href')
+    else:
+        pers_url = get_scholar_profile(name)
+
+    departments = ["Robotics", "Systems and Networking", "Computer Architecture", "Multicore Processors & Cloud Computing"]
+    for department in departments:
+        if department in research_list:
+            print([university, country, text_content, email, indiv_url, pers_url])
+            faculty_data.append([university, country, text_content, email, indiv_url, pers_url])
+            break
+
+def mit():
+    base_url = "https://www.eecs.mit.edu/role/faculty/"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36'
     }
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(base_url, headers=headers)
     html_content = response.text
-
-    # Parse the HTML with BeautifulSoup
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Find all <a> tags with Name: a, Class: None, ID: None, and 'rel': ['bookmark']
-    extract_webpage = soup.find_all('a', class_=None, id=None, rel=['bookmark'])
-
-    departments = ["Robotics", "Systems and Networking", "Computer Architecture", "Multicore Processors & Cloud Computing"]
+    all_names = [element.get('title') for element in soup.find_all('a', class_="people-index-image")]
 
     faculty_data = []
-
-    for element in extract_webpage:
-        text_content = element.get_text()  # Separate text content by newlines for better readability
-        href = element.get('href')  # Get the href attribute value
-        # print("Text Content:", text_content)
-        # print("Href:", href)
-        indiv_url = href
-        new_response = requests.get(indiv_url, headers=headers)
-        new_html_content = new_response.text
-        new_soup = BeautifulSoup(new_html_content, 'html.parser')
-        extract_email = new_soup.find_all('a', class_=None, id=None, href=lambda href: href and href.startswith('mailto:'))
-        email = extract_email[0].get_text()
-        # print("Email:", email)
-        extract_research = new_soup.find_all('a', class_=None, id=None, href=lambda href: href and href.startswith('/people/?fwp_research'))
-        # Print all matches as a list
-        research_list = [link.get_text() for link in extract_research]
-        # print("Research List:", research_list)
-        for department in departments:
-            if department in research_list:
-                print([university, country, text_content, email, href])
-                faculty_data.append([university, country, text_content, email, href])
-                break
-
-
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_faculty_data, name, base_url, headers, faculty_data) for name in all_names]
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                future.result()
+            except Exception as e:
+                print(f"Error occurred: {e}")
     print()
     print("MIT Done....")
     print()
     return faculty_data
-
-# mit()
