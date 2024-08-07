34b6818b5a14a624d5719f8932966dd8e34845dd
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-08-08 05:00:10 +0530

    implemented parallelism for anu

--- main2.py
+++ main2.py
@@ -1,12 +1,12 @@
-from scraper_files.national_taiwan_university import national_taiwan_university
+from scraper_files.anu import anu
 import csv
 
 def main():
     with open('faculty_data.csv', 'a', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["University", "Country", "Name", "Email", "URL", "Personal Webpage"])
-        
-        writer.writerows(national_taiwan_university())
+
+        writer.writerows(anu())
 
 
 if __name__ == "__main__":
