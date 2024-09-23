327346efd7683f592c2824031d75d332177525cf
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-09-23 23:52:04 +0530

    eoruneu

--- scraper_files/oxford.py
+++ scraper_files/oxford.py
@@ -1,10 +1,20 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+import sys
+import os
 import re
+import concurrent.futures
+import pprint
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from components.google_scholar import get_scholar_profile
+from components.GLOBAL_VARIABLES import keyword_list
+
+faculty_data = []
 from unidecode import unidecode
 
-university = "Oxford"
-country = "UK"
+university = "Oxford University"
+country = "United Kingdom"
 
 def normalize_email(email):
     # Replace common variations with '@'
@@ -16,45 +26,61 @@ def normalize_email(email):
     email = re.sub(r'\s*\[\s*dot\s*\]\s*', '.', email)
     return email
 
+def get_name(prof):
+    name = prof.get_text().strip()
+    return name
+
+def get_link(prof):
+    href = "https://www.cs.ox.ac.uk/people/" + unidecode(prof).lower().replace(" ", ".")
+    return href
+
+def get_faculty_data(prof):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_name = executor.submit(get_name, prof)
+        future_link = executor.submit(get_link, prof)
+
+        name = future_name.result()
+        link = future_link.result()
+
+    new_resonse = requests.get(link)
+    new_content = new_resonse.text
+
+    new_soup = BeautifulSoup(new_content, 'html.parser')
+
+    if any(keyword in new_soup.get_text().lower() for keyword in keyword_list):
+        pers_link = get_scholar_profile(name)
+
+        try:
+            email = new_soup.find('div', class_='scaled-text', itemprop='email').get_text()[3:]
+            email = normalize_email(email)
+        except AttributeError:
+            email = "N/A"
+
+        faculty_data.append([university, country, name, email, link, pers_link])
+        print([university, country, unidecode(name), email, link, pers_link])
+
 def oxford():
     url = "https://www.cs.ox.ac.uk/people/faculty.html"
 
     response = requests.get(url)
     html_content = response.text
 
-    keyword_list = ["embedded system", "computer systems", "kernel", "robotics", "operating system", "autonomous system", "human-robot interaction", "coordination", "decision making", "sensor networks"]
-
-    faculty_data = []
-
-    # Parse the HTML with BeautifulSoup
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    extract_webpage = soup.find_all('span', itemprop='name')
-    for element in extract_webpage:
-        name = element.get_text().strip()
-        # print("Name:", name)
-        href = "https://www.cs.ox.ac.uk/people/" + unidecode(name).lower().replace(" ", ".")
-        # print("URL:", href)
+    all_profs = soup.find_all('span', itemprop='name')
 
-        new_resonse = requests.get(href)
-        new_content = new_resonse.text
-
-        new_soup = BeautifulSoup(new_content, 'html.parser')
-
-        if any(keyword in new_soup.get_text().lower() for keyword in keyword_list):
-            # print("Research:", "Yes")
-
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_faculty_data, prof) for prof in all_profs]
+        for future in concurrent.futures.as_completed(futures):
             try:
-                email = new_soup.find('div', class_='scaled-text', itemprop='email').get_text()[3:]
-                email = normalize_email(email)
-            except AttributeError:
-                email = "Email not found"
+                future.result()
+            except Exception as e:
+                print(f"Error occurred: {e}")
 
-            faculty_data.append([university, country, name, email, href])
-            print([university, country, unidecode(name), email, href])
     print()
-    print("Oxford done....")
+    print("Oxford University done....")
     print()
     return faculty_data
 
-# oxford()
+if __name__ == "__main__":
+    oxford()
