c6a45e6504034aeef158eb3729d788e69e14d9b5
akashdabaniya <117190502+akashdabaniya@users.noreply.github.com>
2024-09-26 15:03:06 +0530

    Update ens_lyon.py

--- scraper_files/ens_lyon.py
+++ scraper_files/ens_lyon.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import concurrent.futures
 import sys
 import os
 import re
-import concurrent.futures
+
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from components.google_scholar import get_scholar_profile
