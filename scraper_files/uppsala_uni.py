f68d9de01bdf0335ae04298d826cfb13e56d30db
akashdabaniya <117190502+akashdabaniya@users.noreply.github.com>
2024-09-27 16:31:48 +0530

    Update uppsala_uni.py

--- scraper_files/uppsala_uni.py
+++ scraper_files/uppsala_uni.py
@@ -1,9 +1,10 @@
 import requests
-from bs4 import BeautifulSoup
 import sys
 import os
 import re
 import concurrent.futures
+
+from bs4 import BeautifulSoup
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from components.google_scholar import get_scholar_profile
