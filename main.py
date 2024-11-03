1f1a2821c1788682ee6286f184ddf8b49fcb00bc
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-11-03 22:43:52 +0530

    added swinburne university

--- main.py
+++ main.py
@@ -279,6 +279,7 @@ from scraper_files.norwegian_uni import norwegian_uni
 from scraper_files.uni_macau import uni_macau
 from scraper_files.brunel_uni import brunel_uni
 from scraper_files.uni_victoria import uni_victoria
+from scraper_files.swinburne_uni import swinburne_uni
 
 
 def main():
@@ -504,6 +505,7 @@ def main():
         writer.writerows(uni_macau())
         writer.writerows(brunel_uni())
         writer.writerows(uni_victoria())
+        writer.writerows(swinburne_uni())
 
 
 if __name__ == "__main__":
