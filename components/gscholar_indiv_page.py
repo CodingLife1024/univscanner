003bcd93d90737bb8abbd39d56541a539a2fc4a9
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-11-08 19:51:35 +0530

    added

--- components/gscholar_indiv_page.py
+++ components/gscholar_indiv_page.py
@@ -19,50 +19,53 @@ def search_individual(person, headers, u_name, country, print_flag=0):
     name = person.find('h3', class_='gs_ai_name').text
     link = "https://scholar.google.com" + person.find('a')['href']
     title = person.find('div', class_="gs_ai_aff").text
+    low_title = title.lower()
 
     personal_data = [name, link, title]
 
-    new_r = requests.get(link, headers=headers)
-    new_soup = BeautifulSoup(new_r.text, 'html.parser')
+    if "professor" in low_title or "lecturer" in low_title and "emeritus" not in low_title:
 
-    homepage = new_soup.find('a', string="Homepage")['href'] if new_soup.find('a', string="Homepage") else "N/A"
+        new_r = requests.get(link, headers=headers)
+        new_soup = BeautifulSoup(new_r.text, 'html.parser')
 
-    areas_of_expertise = new_soup.find('a', class_='gsc_prf_inta gs_ibl') if new_soup.find('a', class_='gsc_prf_inta gs_ibl') else []
+        homepage = new_soup.find('a', string="Homepage")['href'] if new_soup.find('a', string="Homepage") else "N/A"
 
-    research_string = "Research:\n" + "\n".join([area.text for area in areas_of_expertise])
-    article_string = "Articles:\n"
+        areas_of_expertise = new_soup.find('a', class_='gsc_prf_inta gs_ibl') if new_soup.find('a', class_='gsc_prf_inta gs_ibl') else []
 
-    all_articles = new_soup.find_all('tr', class_='gsc_a_tr')
+        research_string = "Research:\n" + "\n".join([area.text for area in areas_of_expertise])
+        article_string = "Articles:\n"
 
-    all_articles_info = []
+        all_articles = new_soup.find_all('tr', class_='gsc_a_tr')
 
-    for article in all_articles:
-        title = article.find('a').text
-        link = "https://scholar.google.com" + article.find('a')['href']
-        research_string = research_string + "\n" + title
-        article_string = article_string + "\n" + title
-        all_articles_info.append((title, link))
+        all_articles_info = []
 
-    if print_flag == 1:
-        print("===" * 10)
-        print("\n")
-        print(personal_data)
-        print(research_string)
-        print(article_string)
-        print("\n")
-        print("===" * 10)
+        for article in all_articles:
+            title = article.find('a').text
+            link = "https://scholar.google.com" + article.find('a')['href']
+            research_string = research_string + "\n" + title
+            article_string = article_string + "\n" + title
+            all_articles_info.append((title, link))
 
-    all_research = research_string + "\n" + article_string
+        if print_flag == 1:
+            print("===" * 10)
+            print("\n")
+            print(personal_data)
+            print(research_string)
+            print(article_string)
+            print("\n")
+            print("===" * 10)
 
-    found_keyword = any(re.search(re.escape(keyword), all_research, re.IGNORECASE) for keyword in keyword_list)
+        all_research = research_string + "\n" + article_string
 
-    if found_keyword:
-        author_link = "https://scholar.google.com/citations?hl=en&user=" + link.split("user=")[1].split("&")[0]
-        data = [u_name, country, name, "NA", homepage, author_link]
-        faculty_data.append(data)
-        print(data)
+        found_keyword = any(re.search(re.escape(keyword), all_research, re.IGNORECASE) for keyword in keyword_list)
 
-    return personal_data, homepage, research_string, article_string, all_research
+        if found_keyword:
+            author_link = "https://scholar.google.com/citations?hl=en&user=" + link.split("user=")[1].split("&")[0]
+            data = [u_name, country, name, "NA", homepage, author_link]
+            faculty_data.append(data)
+            print(data)
+
+        return personal_data, homepage, research_string, article_string, all_research
 
 def search_faculty_list(url, headers, u_name, country):
     r = requests.get(url, headers=headers)
